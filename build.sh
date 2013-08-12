3e14ed590d4783375aa9c097b96b586cd70bcb7c
Jaime del Palacio <jaimedelpalacio@forio.com>
2013-08-12 13:24:24 -0700

    more

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@
 export PATH="/usr/local/bin:$PATH"
 npm install
 grunt production
-target="deploy-$BUILD_ENV"
-grunt $target
+if [ -n "$BUILD_ENV" ]; then
+    target="deploy-$BUILD_ENV"
+    grunt $target
+fi
